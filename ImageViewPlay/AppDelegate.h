be3f7bb4983aab3ae3693b32d3b3210f174c3756
wuzhiguang <anbaida@wuzhiguangdemacbook-pro.local>
2018-08-02 10:40:55 +0800

    Initial Commit

---
+++ ImageViewPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageViewPlay
+//
+//  Created by wuzhiguang on 2018/8/2.
+//  Copyright © 2018年 红爵科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageViewPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageViewPlay
+//
+//  Created by wuzhiguang on 2018/8/2.
+//  Copyright © 2018年 红爵科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
